bbaa8f608262872606f14b7a327b747832a12cc9
이지은 <71183687+Jinnycorn@users.noreply.github.com>
2026-08-01 23:56:49 +0900

    Make Ex14 a Bistro-only portfolio scene

--- examples/Ex14_Bistro/Ex14_Bistro.cpp
+++ examples/Ex14_Bistro/Ex14_Bistro.cpp
@@ -7,14 +7,6 @@ int main()
     ApplicationConfig config;
 
     config.models.push_back(
-        ModelConfig("characters/Leonard/Bboy Hip Hop Move.fbx", "Dancer")
-            .setTransform(glm::rotate(
-                glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(-6.719f, 0.21f, -1.860f)),
-                           glm::vec3(0.012f)),
-                glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f)))
-            .setAnimation(true, 0, 1.0f, true));
-
-    config.models.push_back(
         ModelConfig("models/AmazonLumberyardBistroMorganMcGuire/exterior.obj", "Bistro")
             .setBistroModel(true)
             .setTransform(glm::scale(glm::mat4(1.0f), glm::vec3(0.01f))));
