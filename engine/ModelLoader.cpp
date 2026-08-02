89386695f9ed5ed45782401e7309c05ad400fbb2
이지은 <71183687+Jinnycorn@users.noreply.github.com>
2026-08-02 09:00:20 +0900

    Raise Bistro material textures to 512px

--- engine/ModelLoader.cpp
+++ engine/ModelLoader.cpp
@@ -43,7 +43,7 @@ void ModelLoader::loadFromModelFile(const string& modelFilename, bool readBistro
                 model_.textures_.emplace_back(make_unique<Image2D>(model_.ctx_));
                 model_.textures_.back()->createTextureFromImage(
                     prefix + filename, false, model_.textureSRgb_[model_.textures_.size() - 1],
-                    readBistroObj ? 256u : 0u);
+                    readBistroObj ? 512u : 0u);
             }
 
             // Calculate elapsed time
@@ -205,7 +205,7 @@ void ModelLoader::loadFromModelFile(const string& modelFilename, bool readBistro
 
             model_.textures_.back()->createTextureFromImage(
                 prefix + shortFilename, false, model_.textureSRgb_[model_.textures_.size() - 1],
-                readBistroObj ? 256u : 0u);
+                readBistroObj ? 512u : 0u);
         }
     }
 
