8377d346064aee15429ae74527f5af1b3aee0895
이지은 <71183687+Jinnycorn@users.noreply.github.com>
2026-08-05 07:32:24 +0900

    Add third-person look-at camera mode

--- engine/Camera.h
+++ engine/Camera.h
@@ -9,7 +9,7 @@ namespace hlab {
 class Camera
 {
   public:
-    enum CameraType { lookat, firstperson };
+    enum CameraType { lookat, firstperson, thirdperson };
 
   public:
     float fov;
@@ -22,6 +22,7 @@ class Camera
     glm::vec3 rotation = glm::vec3(-1.888507, -0.764950, -0.725987); // For Bistro model
     glm::vec3 position = glm::vec3(6.000000, -62.000000, 0.000000);
     glm::vec3 viewPos = glm::vec3(1.888507, -0.764950, 0.725987);
+    glm::vec3 target = glm::vec3(0.0f);
 
     float rotationSpeed = 0.1f;
     float movementSpeed = 10.0f;
@@ -57,6 +58,7 @@ class Camera
     void setPosition(glm::vec3 position);
     void setRotation(glm::vec3 rotation);
     void setViewPos(glm::vec3 viewPos);
+    void setLookAt(glm::vec3 eye, glm::vec3 target);
     void rotate(glm::vec3 delta);
     void setTranslation(glm::vec3 translation);
     void translate(glm::vec3 delta);
